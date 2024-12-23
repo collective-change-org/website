7759b4961437e0d3c03b46d1d368464110360fb7
Eric Wätke <eric.waetke@fh-potsdam.de>
2024-12-23 21:32:36 +0100

    chore: Added SolidJS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,35 +3,35 @@ import starlight from '@astrojs/starlight';
 
 import tailwind from "@astrojs/tailwind";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-	env: {
-		schema: {
-			KIRBY_USERNAME: envField.string({ context: "server", access: "secret" }),
-			KIRBY_PASSWORD: envField.string({ context: "server", access: "secret" }),
-			KIRBY_URL: envField.string({ context: "server", access: "public" }),
-			MOCKDATA: envField.boolean({ context: "server", access: "public", optional: true, default: false }),
-		},
-	},
-	integrations: [
-		starlight({
-			title: 'Capslock 2',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			components: {
-				MarkdownContent: './src/components/MarkdownContent.astro',
-				PageSidebar: './src/components/PageSidebar.astro',
-				Sidebar: './src/components/Sidebar.astro',
-			},
-			customCss: ["./src/tailwind.css"]
-		}),
-		tailwind({
-			applyBaseStyles: false,
-		})],
-	site: 'https://changecollective.woven.design',
-	server: {
-		port: 3000,
-		host: true
-	}
+    env: {
+        schema: {
+            KIRBY_USERNAME: envField.string({ context: "server", access: "secret" }),
+            KIRBY_PASSWORD: envField.string({ context: "server", access: "secret" }),
+            KIRBY_URL: envField.string({ context: "server", access: "public" }),
+            MOCKDATA: envField.boolean({ context: "server", access: "public", optional: true, default: false }),
+        },
+    },
+    integrations: [starlight({
+        title: 'Capslock 2',
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        components: {
+            MarkdownContent: './src/components/MarkdownContent.astro',
+            PageSidebar: './src/components/PageSidebar.astro',
+            Sidebar: './src/components/Sidebar.astro',
+        },
+        customCss: ["./src/tailwind.css"]
+		}), tailwind({
+        applyBaseStyles: false,
+		}), solidJs()],
+    site: 'https://changecollective.woven.design',
+    server: {
+        port: 3000,
+        host: true
+    }
 });
