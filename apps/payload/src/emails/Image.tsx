bc33aaa01982034f47e636019d829839d7a63ffd
Eric Wätke <ewaetke10@gmail.com>
2025-02-22 10:53:34 +0100

    fix: removed faulty image import

--- apps/payload/src/emails/Image.tsx
+++ apps/payload/src/emails/Image.tsx
@@ -1,17 +1,5 @@
-import { Img } from "@react-email/components";
+import { Img } from '@react-email/components'
 
-export const Email = (props: {
-	scr: string;
-	alt: string;
-	width: number;
-	height: number;
-}) => {
-	return (
-		<Img
-			src={props.scr}
-			alt={props.alt}
-			width={props.width}
-			height={props.height}
-		/>
-	);
-};
+export const Image = (props: { scr: string; alt: string; width: number; height: number }) => {
+  return <Img src={props.scr} alt={props.alt} width={props.width} height={props.height} />
+}
