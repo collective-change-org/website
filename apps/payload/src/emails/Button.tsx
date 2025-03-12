900fc724b93d6d22f8a1ea7c1bdd2eadb60867c7
silasflow <93256160+silasflow@users.noreply.github.com>
2025-03-12 22:30:58 +0100

    button & menu tweaked

--- apps/payload/src/emails/Button.tsx
+++ apps/payload/src/emails/Button.tsx
@@ -1,28 +1,28 @@
-import { Button, Text } from '@react-email/components'
-import React from 'react'
+import { Button, Text } from "@react-email/components";
+import React from "react";
 
 export const EmailButton = (props: {
-  href: string
-  children: React.ReactNode
-  color: 'green' | 'orange' | 'black'
-  size: 'small' | 'large'
+  href: string;
+  children: React.ReactNode;
+  color: "green" | "orange" | "black";
+  size: "small" | "large";
 }) => {
-  const color =
-    props.color === 'green'
-      ? 'bg-green-lighter hover:bg-green-dark text-white'
-      : props.color === 'orange'
-        ? 'bg-orange-light hover:bg-orange-dark text-black'
-        : 'bg-green-black hover:bg-black text-white'
+  const color = props.color === "green"
+    ? "bg-green-lighter hover:bg-green-dark text-white"
+    : props.color === "orange"
+    ? "bg-orange-light hover:bg-orange-dark text-black"
+    : "bg-green-black hover:bg-black text-white";
 
-  const size =
-    props.size === 'small' ? 'text-base px-[20px] py-[10px]' : 'text-2xl h-[56px] px-[24px]'
+  const size = props.size === "small"
+    ? "typestyle-text-s font-medium px-[20px] py-[10px]"
+    : "typestyle-text-l font-medium h-[56px] px-[24px]";
 
   return (
     <Button
       href={props.href}
-      className={`inline-block min-w-fit cursor-pointer rounded-full border-2 border-white/10 uppercase no-underline ${color} ${size}`}
+      className={`inline-block min-w-fit cursor-pointer rounded-full border-2 border-white/10 uppercase no-underline hover:scale-[102%] active:scale-[98%] transition-all duration-200 ${color} ${size}`}
     >
       <Text className="m-auto">{props.children}</Text>
     </Button>
-  )
-}
+  );
+};
--- apps/website/src/components/button/button.ts
+++ apps/website/src/components/button/button.ts
@@ -2,19 +2,19 @@ import { cva } from "cva";
 
 export const button = cva({
 	base:
-		"rounded-full ring-white/10 ring-2 ring-inset uppercase flex items-center gap-1.5 cursor-pointer no-underline min-w-fit",
+		"rounded-full ring-white/10 ring-2 ring-inset uppercase flex items-center gap-1.5 cursor-pointer no-underline min-w-fit hover:scale-[102%] active:scale-[98%] transition-all duration-200",
 	variants: {
 		intent: {
 			green: ["bg-green-lighter hover:bg-green-dark text-white"],
 			black: ["bg-green-black hover:bg-black text-white"],
 			orange: ["bg-orange-light hover:bg-orange-dark text-black"],
 			red: [
-				"bg-transparent hover:bg-red-light hover:text-black text-red-light font-medium ring-0",
+				"bg-transparent hover:bg-red-light hover:text-black text-red-light ring-0",
 			],
 		},
 		size: {
-			small: ["text-base", "h-11", "px-5"],
-			large: ["text-2xl", "h-14", "px-6"],
+			small: ["typestyle-text-m font-medium", "h-11", "px-5"],
+			large: ["typestyle-text-l font-medium", "h-14", "px-6"],
 		},
 		disabled: {
 			false: null,
