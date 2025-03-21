86ce36c201ba82eadf34502a6ec246ef3e3a4fa9
Eric Wätke <eric.waetke@fh-potsdam.de>
2025-03-21 16:33:30 +0100

    chore: Renamed manifest folder

--- apps/payload/src/blocks/Containers/container.ts
+++ apps/payload/src/blocks/Containers/container.ts
@@ -7,13 +7,14 @@ import { H1Block } from "../headings/h1"
 import { H2Block } from "../headings/h2"
 import { HeroBlock } from "../hero"
 import { LoginBlock } from "../login"
-import { ManifestBlock } from "../manifest/config"
+
 import { MediaBlock } from "../MediaBlock/config"
 import { LargeRichTextBlock } from "../RichText/large-rich-text-block"
 import { SignupBlock } from "../sign-up"
 import { UpcomingEvents } from "../upcoming-events"
 import { ColumnContainerBlock } from "./column-container"
 import { IndentedContainer } from "./indented-container"
+import { ManifestBlock } from "../manifest/config"
 
 export const ContainerBlock: Block = {
 	slug: "containerBlock",
